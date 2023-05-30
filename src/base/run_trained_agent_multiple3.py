d353b34fde2114e266bf31645fb572eb37130406
johnny <jonathan.espinola@ug.uchile.cl>
2023-05-29 20:08:43 -0400

    Change to test v3 RPF

--- src/base/run_trained_agent_multiple3.py
+++ src/base/run_trained_agent_multiple3.py
@@ -42,10 +42,10 @@ rcParams["pdf.fonttype"] = 42  # To avoid Type 3 fonts in figures
 rcParams["ps.fonttype"] = 42
 
 """ Options: """
-filepath = "../logs/train_agent_20230323_235314_dqn_6M_v3/"#"../logs/train_agent_20230323_235219_rpf_6M_v3/", "../logs/train_agent_20230323_235314_dqn_6M_v3/"
-agent_name = "5950056"#rpf: "5950033", dqn: "5950056"
-case = "rerun_test_scenarios"  # 'rerun_test_scenarios', 'fast_overtaking', 'standstill', 'all'
-use_ensemble_test_policy = False
+filepath = "../logs/train_agent_20230323_235219_rpf_6M_v3/"#"../logs/train_agent_20230323_235219_rpf_6M_v3/", "../logs/train_agent_20230323_235314_dqn_6M_v3/"
+agent_name = "5950033"#rpf: "5950033", dqn: "5950056"
+case = "all"  # 'rerun_test_scenarios', 'fast_overtaking', 'standstill', 'all'
+use_ensemble_test_policy = True
 
 thresh_range = np.linspace(0, 0.1, 100)
 save_video = False
